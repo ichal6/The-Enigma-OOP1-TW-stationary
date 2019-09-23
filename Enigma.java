3534cc47a8aeb705f177dc04a726f737a98408f2
Wojciech Kapko <wojciech.kapko@gmail.com>
2019-09-23 04:35:18 -0700

    test change

--- Enigma.java
+++ Enigma.java
@@ -14,7 +14,7 @@ public class Enigma {
         }
 
         switch(mode){
-            case "-e":
+            case "-x":
             case "-d":
                 loadCipher(mode, cipher, key);
                 System.out.println("Do you want to continue?[Y]");
