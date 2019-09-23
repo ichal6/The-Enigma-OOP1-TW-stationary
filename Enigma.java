c76c1c3e2b8e310b2e2ee1dad3b86af764fcf549
Michael Lechowicz <ichal.lechowicz@gmail.com>
2019-09-23 16:36:59 +0200

    Repair deCipher homophoniccipher

--- Enigma.java
+++ Enigma.java
@@ -4,13 +4,13 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.Random;
 
 
 public class Enigma {
     public static void main(String[] args) {
         System.out.println("The Enigma");
-        homophonicCipher("szkielet","-e");
-/*
+
         String mode = args[0];
         String cipher = args[1];
         String key = "";
@@ -37,7 +37,7 @@ public class Enigma {
             default:
                 System.out.println("Option not supported. Try: -e | -d | -l");
         }
-*/
+
     }
 
     private static void listAvailableCiphers() {
@@ -75,13 +75,81 @@ public class Enigma {
 
     private static void homophonicCipher(String text, String mode)
     {
-        
-        if(mode.equals("-e")){
+        Random rand = new Random();
+        int randomKey = rand.nextInt(3);
 
-    }
-        else{
-            System.out.println(text);
+        text = text.toUpperCase();
+        char[] textAsChar = text.toCharArray();
+        String alphabet = "ABCDEFGHIJKLMNOPRSTUVWXYZ";
+        char[] alphabetAsChar = alphabet.toCharArray();
+        String[] keys = {"DXSFZEHCVITPGAQLKJRUOWMYBN", "9XSF7EHC3ITPG50LKJ46OWMYBN", "DXSF2EHCVITPGAQLKJRUOWMYBN", "DXSF1EHCVITPGAQLKJRUOWMYBN"};
+        String key = keys[randomKey];
+        char[] keyAsChar = key.toCharArray();
+        String returnText = "";
+        System.out.println(randomKey);
+        if(mode.equals("-e"))
+        {
+            
+            for(char letter: textAsChar)
+            {
+                int index = 0;
+                while(index < alphabet.length())
+                {
+                    if(letter == alphabetAsChar[index])
+                    {
+                        returnText += key.charAt(index);
+                        break;
+                    }
+                    index++;
+
+                    if(index == alphabet.length())
+                    {
+                        returnText += letter;
+                    }
+                
+                }
+                
+            }
+            System.out.println(returnText);   
+            
         }
-    }
+        else
+        {
+            int indexKeys = 0;
+            key = keys[indexKeys];
+            keyAsChar = key.toCharArray();
+            for(char letter: textAsChar)
+            {
+                int index = 0;
+                indexKeys = 0;
+                while(index < alphabet.length())
+                {
+                    if(letter == keyAsChar[index])
+                    {
+                        returnText += alphabet.charAt(index);
+                        indexKeys = 0;
+                        break;
+                    }
+                    index++;
 
+                    if(indexKeys > 2)
+                    {
+                        returnText += letter;
+                        break;
+                    }
+
+                    if(index == alphabet.length())
+                    {
+                        key = keys[++indexKeys];
+                        keyAsChar = key.toCharArray();
+                        index = 0;
+                    }
+                }
+                
+            }
+             System.out.println(returnText);  
+        }
+
+
+    }
 }
