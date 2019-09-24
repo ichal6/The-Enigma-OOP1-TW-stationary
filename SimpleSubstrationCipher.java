b94b2a824511fd400b1609dc83f27fcb91be0198
Michael Lechowicz <ichal.lechowicz@gmail.com>
2019-09-24 13:24:23 +0200

    Refactor and rename class SimpleSubstration

---
+++ SimpleSubstrationCipher.java
@@ -0,0 +1,83 @@
+public class SimpleSubstrationCipher {
+    public static void main(String[] args) {
+        simpleSubstitutionCipher("test", "-e");
+        simpleSubstitutionCipher("RZJR", "-d");
+    }
+
+    public static void simpleSubstitutionCipher(String text, String mode)
+    {
+        text = text.toUpperCase();
+        char[] textAsChar = text.toCharArray();
+
+        String alphabet = "ABCDEFGHIJKLMNOPRSTUVWXYZ";
+
+        String key = "DXSFZEHCVITPGAQLKJRUOWMYBN";
+
+        String returnText = "";
+
+        if(mode.equals("-e"))
+        {
+            returnText = simpleSubstationEncripher(alphabet, textAsChar, key);
+            System.out.println(returnText);   
+        }
+        else
+        {
+            returnText = simpleSubstationDecripher(alphabet, textAsChar, key);
+            System.out.println(returnText);  
+        }
+    }
+
+    private static String simpleSubstationEncripher(String alphabet, char[] textAsChar, String key)
+    {
+        char[] alphabetAsChar = alphabet.toCharArray();
+        String returnText = "";
+        for(char letter: textAsChar)
+        {
+            int index = 0;
+            while(index < alphabet.length())
+            {
+                if(letter == alphabetAsChar[index])
+                {
+                    returnText += key.charAt(index);
+                    break;
+                }
+                index++;
+
+                if(index == alphabet.length())
+                {
+                    returnText += letter;
+                }
+            
+            }
+            
+        }
+        return returnText;
+    }
+
+    private static String simpleSubstationDecripher(String alphabet, char[] textAsChar, String key)
+    {
+        String returnText = "";
+        char[] keyAsChar = key.toCharArray();
+        for(char letter: textAsChar)
+            {
+                int index = 0;
+                while(index < alphabet.length())
+                {
+                    if(letter == keyAsChar[index])
+                    {
+                        returnText += alphabet.charAt(index);
+                        break;
+                    }
+                    index++;
+
+                    if(index == alphabet.length())
+                    {
+                        returnText += letter;
+                    }
+                
+                }
+                
+            }
+        return returnText;
+    }
+}
