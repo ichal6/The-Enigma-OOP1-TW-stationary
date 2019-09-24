b6421b747744baa7fa9d43762d26135dd51b96c1
Michael Lechowicz <ichal.lechowicz@gmail.com>
2019-09-24 11:18:56 +0200

    initialize Array in array

--- Enigma.java
+++ Enigma.java
@@ -11,6 +11,8 @@ public class Enigma {
     public static void main(String[] args) {
         System.out.println("The Enigma");
 
+        homophonicCipherImprove("something", "-e");
+/*
         String mode = args[0];
         String cipher = args[1];
         String key = "";
@@ -37,7 +39,7 @@ public class Enigma {
             default:
                 System.out.println("Option not supported. Try: -e | -d | -l");
         }
-
+*/
     }
 
     private static void listAvailableCiphers() {
@@ -82,7 +84,8 @@ public class Enigma {
         char[] textAsChar = text.toCharArray();
         String alphabet = "ABCDEFGHIJKLMNOPRSTUVWXYZ";
         char[] alphabetAsChar = alphabet.toCharArray();
-        String[] keys = {"DXSFZEHCVITPGAQLKJRUOWMYBN", "9XSF7EHC3ITPG50LKJ46OWMYBN", "DXSF2EHCVITPGAQLKJRUOWMYBN", "DXSF1EHCVITPGAQLKJRUOWMYBN"};
+        String[] keys = {"DXSFZEHCVITPGAQLKJRUOWMYBN", "9XSF7EHC3ITPG50LKJ46OWMYBN",
+                         "DXSF2EHCVITPGAQLKJRUOWMYBN", "DXSF1EHCVITPGAQLKJRUOWMYBN"};
         String key = keys[randomKey];
         char[] keyAsChar = key.toCharArray();
         String returnText = "";
@@ -150,6 +153,118 @@ public class Enigma {
              System.out.println(returnText);  
         }
 
+
+    }
+
+    private static void homophonicCipherImprove(String text, String mode)
+    {
+        char[] textAsChar = text.toCharArray();
+        String alphabet = "ABCDEFGHIJKLMNOPRSTUVWXYZ";
+        char[] alphabetAsChar = alphabet.toCharArray();
+        ArrayList<ArrayList<String>> keysAsArrays = new ArrayList<ArrayList<String>>();
+        String keyAsString = "DXSFZEHCVITPGAQLKJRUOWMYBN";
+        String[] keyByOne = keyAsString.split("");
+        keyByOne[0] = keyByOne[0].concat("9");
+        keyByOne[4] = keyByOne[4].concat("7");
+        keyByOne[4] = keyByOne[4].concat("2");
+        keyByOne[4] = keyByOne[4].concat("1");
+        keyByOne[8] = keyByOne[8].concat("3");
+        keyByOne[13] = keyByOne[13].concat("5");
+        keyByOne[14] = keyByOne[14].concat("0");
+        keyByOne[18] = keyByOne[18].concat("4");
+        keyByOne[19] = keyByOne[19].concat("6");
+        
+
+        for(int index = 0; index < keyAsString.length(); index++)
+        {
+            ArrayList<String> keyAsArray = new ArrayList<String>();
+            keyAsArray.add(keyByOne[index]);
+            keysAsArrays.add(keyAsArray);
+        }
+        System.out.println(keysAsArrays);
+
+        for (char letter: textAsChar)
+        {
+            if()
+        }
+        /*
+
+        Random rand = new Random();
+        int randomKey = rand.nextInt(3);
+
+        text = text.toUpperCase();
+        char[] textAsChar = text.toCharArray();
+        
+        char[] alphabetAsChar = alphabet.toCharArray();
+        /keys = {"DXSFZEHCVITPGAQLKJRUOWMYBN", "9XSF7EHC3ITPG50LKJ46OWMYBN",
+                         "DXSF2EHCVITPGAQLKJRUOWMYBN", "DXSF1EHCVITPGAQLKJRUOWMYBN"};
+        String key = keys[randomKey];
+        char[] keyAsChar = key.toCharArray();
+        String returnText = "";
+        System.out.println(randomKey);
+        if(mode.equals("-e"))
+        {
+            
+            for(char letter: textAsChar)
+            {
+                int index = 0;
+                while(index < alphabet.length())
+                {
+                    if(letter == alphabetAsChar[index])
+                    {
+                        returnText += key.charAt(index);
+                        break;
+                    }
+                    index++;
+
+                    if(index == alphabet.length())
+                    {
+                        returnText += letter;
+                    }
+                
+                }
+                
+            }
+            System.out.println(returnText);   
+            
+        }
+        else
+        {
+            int indexKeys = 0;
+            key = keys[indexKeys];
+            keyAsChar = key.toCharArray();
+            for(char letter: textAsChar)
+            {
+                int index = 0;
+                indexKeys = 0;
+                while(index < alphabet.length())
+                {
+                    if(letter == keyAsChar[index])
+                    {
+                        returnText += alphabet.charAt(index);
+                        indexKeys = 0;
+                        break;
+                    }
+                    index++;
+
+                    if(indexKeys > 2)
+                    {
+                        returnText += letter;
+                        break;
+                    }
+
+                    if(index == alphabet.length())
+                    {
+                        key = keys[++indexKeys];
+                        keyAsChar = key.toCharArray();
+                        index = 0;
+                    }
+                }
+                
+            }
+             System.out.println(returnText);  
+        }
+        */
 
     }
 }
