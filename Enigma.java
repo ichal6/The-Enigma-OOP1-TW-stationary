72284d723c878ce26a5f846e1d57c0eb8871e7c0
Wojciech Kapko <wojciech.kapko@gmail.com>
2019-09-23 23:23:49 -0700

    half done

--- Enigma.java
+++ Enigma.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class Enigma {
     public static void main(String[] args) {
@@ -47,7 +50,7 @@ public class Enigma {
                 atbashCipher(text, mode);
                 break;
             case "ADFGX":
-                adfgxCipher(text, mode);
+                adfgxCipher(text, mode, key);
                 break;
             default:
                 System.out.println("Cipher not supported");
@@ -70,13 +73,57 @@ public class Enigma {
         return text;
     }
 
-    private static void adfgxCipher(String text, String mode) {
+    private static void adfgxCipher(String text, String mode, String key) {
+        char[][] keySquare = {{'p', 'h', 'q', 'g', 'm'}, {'e', 'a', 'y', 'n', 'o'}, {'f', 'd', 'x', 'k', 'r'}, {'c', 'v', 's', 'z', 'w'}, {'b', 'u', 't', 'i', 'l'}};
+        
+        List<Character> result = new ArrayList<>();
+        HashMap<Integer, Character> hashMap = new HashMap<>();
+        hashMap.put(0, 'A');
+        hashMap.put(1, 'D');
+        hashMap.put(2, 'F');
+        hashMap.put(3, 'G');
+        hashMap.put(4, 'X');
+
         if(mode.equals("-e")){
-            System.out.println(text);
+            for (int i = 0; i < text.length(); i++) {
+                result.add(getY(hashMap, keySquare, text.charAt(i)));
+                result.add(getX(hashMap, keySquare, text.charAt(i)));
+            }
+            System.out.println(result);
+            System.out.println(key);
+            while (result.size() != 0){
+                char x = result.get(0);
+                result.remove(0);
+                char y = result.get(0);
+                char[] temp = {}
+            }
+            
+
         }
         else{
             System.out.println(text);
         }
+    }
+
+    private static char getY( HashMap<Integer, Character> hashMap, char[][] keySquare,char character_){
+
+        for (char[] temp: keySquare){
+            if(new String(temp).indexOf(character_) != -1){
+                return hashMap.get(Arrays.asList(keySquare).indexOf(temp));
+            }
+        }
+        return '-';
+    }
+
+    private static char getX( HashMap<Integer, Character> hashMap, char[][] keySquare,char character_){
+
+        for (char[] temp: keySquare){
+            int index_ = new String(temp).indexOf(character_);
+            if(index_ != -1){
+                return hashMap.get(index_);
+            }
+        }
+        return '-';
     }
 
 }
