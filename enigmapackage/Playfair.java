077fb559a69d43bb31ef683bd1b37c4b88747063
Wojciech Kapko <wojciech.kapko@gmail.com>
2019-09-26 23:44:26 +0200

    playfair decryption

--- enigmapackage/Playfair.java
+++ enigmapackage/Playfair.java
@@ -28,23 +28,30 @@ public class Playfair {
         }
 
         // adding 'x' at the end if needed
-        if (text.length() % 2 != 0) {
-            text += "X";
+        if(mode.equals("-e")){
+            if (text.length() % 2 != 0) {
+                text += "X";
+            }
         }
-
         // dividing into pairs
         String[] textPairs = splitTextIntoPairs(text);
 
-        // replacing second double char with 'x'
-        for(String pair: textPairs) {
-            if(pair.charAt(0) == pair.charAt(1)){
-                String newPair = "" + pair.charAt(0) + 'X';
-                textPairs[Arrays.asList(textPairs).indexOf(pair)] = newPair;
-            }
-        }
-
         // main algorythm
-        encode(textPairs, cipherTable);
+        if(mode.equals("-e")){
+            // replacing second double char with 'x'
+            for(String pair: textPairs) {
+                if(pair.charAt(0) == pair.charAt(1)){
+                    String newPair = "" + pair.charAt(0) + 'X';
+                    textPairs[Arrays.asList(textPairs).indexOf(pair)] = newPair;
+                }
+            }
+            // Encrypt
+            encrypt(textPairs, cipherTable);
+        }
+        else{
+            // Decrypt
+            decrypt(textPairs, cipherTable);
+        }
     }
 
     private static void generateCipherMatrix(String[][] cipherTable, String cipherKey) {
@@ -64,7 +71,7 @@ public class Playfair {
         return textPairs;
     }
 
-    private static void encode(String[] letterPairs, String[][] cipherTable) {
+    private static void decrypt(String[] letterPairs, String[][] cipherTable) {
         String result = "";
         for (String pair: letterPairs){
             String char1 = "" + pair.charAt(0);
@@ -74,11 +81,40 @@ public class Playfair {
                 result += findRow(char1, cipherTable)[Arrays.asList(findRow(char2, cipherTable)).indexOf(char2)];
                 result += findRow(char2, cipherTable)[Arrays.asList(findRow(char1, cipherTable)).indexOf(char1)];
             } else if(checkRow(char1, char2, cipherTable)){
-                result += findRow(char1, cipherTable)[calculateIndex(findRow(char1, cipherTable), char1)];
-                result += findRow(char2, cipherTable)[calculateIndex(findRow(char2, cipherTable), char2)];
+                result += findRow(char1, cipherTable)[calculateIndex("-d", findRow(char1, cipherTable), char1)];
+                result += findRow(char2, cipherTable)[calculateIndex("-d", findRow(char2, cipherTable), char2)];
             } else if(checkColumn(char1, char2, cipherTable)){
-                result += findColumn(char1, cipherTable)[Arrays.asList(findRow(char1, cipherTable)).indexOf(char1)];
-                result += findColumn(char2, cipherTable)[Arrays.asList(findRow(char2, cipherTable)).indexOf(char2)];
+                result += findColumn("-d", char1, cipherTable)[Arrays.asList(findRow(char1, cipherTable)).indexOf(char1)];
+                result += findColumn("-d", char2, cipherTable)[Arrays.asList(findRow(char2, cipherTable)).indexOf(char2)];
+            }
+        }
+        // Remove X from the end
+        if (result.charAt(result.length()-1) == 'X') {
+            result = result.substring(0, result.length() - 1);
+        }
+        // Restore double letters
+        if (result.indexOf("X") != -1) {
+            result = result.replace('X', result.charAt(result.indexOf("X")-1));
+        }
+
+        System.out.println("Decoded text: " + result);
+    }
+
+    private static void encrypt(String[] letterPairs, String[][] cipherTable) {
+        String result = "";
+        for (String pair: letterPairs){
+            String char1 = "" + pair.charAt(0);
+            String char2 = "" + pair.charAt(1);
+
+            if((!checkRow(char1, char2, cipherTable)) && (!checkColumn(char1, char2, cipherTable))){
+                result += findRow(char1, cipherTable)[Arrays.asList(findRow(char2, cipherTable)).indexOf(char2)];
+                result += findRow(char2, cipherTable)[Arrays.asList(findRow(char1, cipherTable)).indexOf(char1)];
+            } else if(checkRow(char1, char2, cipherTable)){
+                result += findRow(char1, cipherTable)[calculateIndex("-e", findRow(char1, cipherTable), char1)];
+                result += findRow(char2, cipherTable)[calculateIndex("-e", findRow(char2, cipherTable), char2)];
+            } else if(checkColumn(char1, char2, cipherTable)){
+                result += findColumn("-e", char1, cipherTable)[Arrays.asList(findRow(char1, cipherTable)).indexOf(char1)];
+                result += findColumn("-e", char2, cipherTable)[Arrays.asList(findRow(char2, cipherTable)).indexOf(char2)];
             }
         }
         System.out.println("Encoded text: " + result);
@@ -94,22 +130,43 @@ public class Playfair {
         return empty;
     }
 
-    private static String[] findColumn(String character, String[][] cipherTable) {
-        if((Arrays.asList(cipherTable).indexOf(findRow(character, cipherTable)) + 1) < cipherTable.length){
-            return cipherTable[Arrays.asList(cipherTable).indexOf(findRow(character, cipherTable)) + 1];
-        } else {
-            return cipherTable[0];
+    private static String[] findColumn(String mode, String character, String[][] cipherTable) {
+        switch(mode){
+            case "-e":
+                if((Arrays.asList(cipherTable).indexOf(findRow(character, cipherTable)) + 1) < cipherTable.length){
+                    return cipherTable[Arrays.asList(cipherTable).indexOf(findRow(character, cipherTable)) + 1];
+                } else {
+                    return cipherTable[0];
+                }
+            case "-d":
+                if(Arrays.asList(cipherTable).indexOf(findRow(character, cipherTable)) != 0){
+                    return cipherTable[Arrays.asList(cipherTable).indexOf(findRow(character, cipherTable)) - 1];
+                } else {
+                    return cipherTable[4];
+                }
         }
+        String[] empty = {""};
+        return empty;
     }
 
 
 
-    private static int calculateIndex(String[] row, String character) {
-        if(Arrays.asList(row).indexOf(character) + 1 < row.length){
-            return Arrays.asList(row).indexOf(character) + 1;
-        } else {
-            return 0;
+    private static int calculateIndex(String mode, String[] row, String character) {
+        switch(mode){
+            case "-e":
+                if(Arrays.asList(row).indexOf(character) + 1 < row.length){
+                    return Arrays.asList(row).indexOf(character) + 1;
+                } else {
+                    return 0;
+                }
+            case "-d":
+                if(Arrays.asList(row).indexOf(character) != 0){
+                    return Arrays.asList(row).indexOf(character) - 1;
+                } else {
+                    return 4;
+                }
         }
+        return -1;
     }
 
 
